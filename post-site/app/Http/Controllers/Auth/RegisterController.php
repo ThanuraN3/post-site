ad3b4eea8f73d798e505a9b8a527e509d071e3b8
Thanura Navodya <59390864+ThanuraN3@users.noreply.github.com>
2022-09-24 10:13:57 +0530

    first update

--- post-site/app/Http/Controllers/Auth/RegisterController.php
+++ post-site/app/Http/Controllers/Auth/RegisterController.php
@@ -12,4 +12,21 @@ class RegisterController extends Controller
         return view('auth.register ');
 
     }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|max:255',
+            'username' => 'required|max:255', 
+            'email' => 'required|email|max:255',
+            'password' => 'required|confirmed',
+        ]);
+
+        User::create([
+            'name' => $request->name,
+            'username' => $request->username,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+    }
 }
